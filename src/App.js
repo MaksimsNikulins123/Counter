9c1c6822a1b5cbe951495684840e12924071e6f3
Max <nikulins.maksims@gmail.com>
2024-11-15 12:35:37 +0200

    Ready fo deploy

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import './index.scss';
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
+  return (
+    <div className="app">
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>- Minus</button>
+        <button className="plus" onClick={onClickPlus}>Plus +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
